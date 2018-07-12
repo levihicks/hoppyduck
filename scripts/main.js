9c0d9cfbe1fae0c3cb8dd7d41ffd47113f477d4a
Levi Hicks <lv.hicks@gmail.com>
2018-07-12 11:59:08 -0500

    create shape parent object for hopper, rename hopper1 variable to duck

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,10 @@ var hopperColor =  "#42c2f4";
 var windowWidth = 900;
 var windowHeight = 600;
 var keyIsDown=false;
-var isHopping = false;
-var hopPeak;
-var hopper1 = new hopper(hopperWidth, hopperHeight, hopperPosX, hopperPosY, hopperColor);
 
-function hopper(width, height, posX, posY, color){
+
+
+function shape(width, height, posX, posY, color){
 	this.width = width;
 	this.height = height;
 	this.posX = posX;
@@ -22,40 +21,50 @@ function hopper(width, height, posX, posY, color){
 	this.color = color;
 }
 
+function hopper(width, height, posX, posY, color){
+	shape.call(this,width, height, posX, posY, color);
+	this.isHopping = false;
+	this.hopPeak;
+}
+
+hopper.prototype = Object.create(shape.prototype);
+hopper.prototype.constructor = hopper;
+
+var duck = new hopper(hopperWidth, hopperHeight, hopperPosX, hopperPosY, hopperColor);
+
 hopper.prototype.draw = function(){
 	ctx.fillStyle = this.color;
 	ctx.fillRect(this.posX, this.posY, this.width, this.height);
-}
+};
+
 window.onkeydown=function(e){
 	if (e.keyCode==32 && keyIsDown == false){
 		keyIsDown = true;
-		isHopping = true;
-		hopPeak = hopper1.posY-60;
+		duck.isHopping = true;
+		duck.hopPeak = duck.posY-60;
 	}
-}
+};
 
 window.onkeyup=function(e){
 	if (e.keyCode==32)
 		keyIsDown = false;
-}
+};
 
 hopper.prototype.fall = function(){
-	if (!isHopping)
+	if (!this.isHopping)
 		this.posY+=3;
-	else if (this.posY<=hopPeak)
-		isHopping=false;
+	else if (this.posY<=this.hopPeak)
+		this.isHopping=false;
 	else
 		this.posY-=20;
 };
 
-
-
 function loop(){
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, windowWidth, windowHeight);
-	hopper1.fall();
-	hopper1.draw();
-	requestAnimationFrame(loop);
+	duck.fall();
+	duck.draw();
+	//requestAnimationFrame(loop);
 }
 
 loop();
